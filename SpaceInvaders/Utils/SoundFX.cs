091774f8d99d325ff0c85a95a5e2de259c8f9932
Phillip Morris <pmorris4@my.westga.edu>
2016-10-26 20:39:33 -0400

    Started working on soundFX

---
+++ SpaceInvaders/Utils/SoundFX.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SpaceInvaders.Utils
+{
+    class SoundFX
+    {
+        public SoundFX()
+        {
+            await playBackgroundMusic();
+        }
+
+        private static async Task playBackgroundMusic()
+        {
+            StorageFolder Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            Folder = await Folder.GetFolderAsync("MyFolder");
+            StorageFile sf = await Folder.GetFileAsync("MyFile.mp3");
+            PlayMusic.SetSource(await sf.OpenAsync(FileAccessMode.Read), sf.ContentType);
+            PlayMusic.Play();
+        }
+    }
+}
