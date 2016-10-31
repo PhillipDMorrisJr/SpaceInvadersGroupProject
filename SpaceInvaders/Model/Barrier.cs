8a4573f94f69142d224cfe1473b04e5e504ad684
Ariel Scott <ascott8@my.westga.edu>
2016-10-31 19:12:45 -0400

    Updated Barrier.cs to run

--- SpaceInvaders/Model/Barrier.cs
+++ SpaceInvaders/Model/Barrier.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using SpaceInvaders.View.Sprites;
+﻿using SpaceInvaders.View.Sprites;
 
 namespace SpaceInvaders.Model
 {
-
+    /// <summary>
+    ///      Manages the Barrier class.
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.Model.GameObject" />
     public class Barrier : GameObject
     {
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Barrier"/> class.
+        /// </summary>
         public Barrier()
         {
             Sprite = new BarrierSprite();
-        }
-
-        public void RemoveTopLeftBarrierPiece()
-        {
-            this.Sprite.;
         }
     }
 }
