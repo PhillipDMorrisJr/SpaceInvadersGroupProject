f3e95482d4c1eec23df03f1f7336ef9f6c4c1654
csuser <csuser@CS1117W15>
2016-10-31 13:56:09 -0400

    Initial commit for Highscore developer branch

---
+++ SpaceInvaders/Model/HighScoreBoard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace SpaceInvaders.Model
+{
+    class HighScoreBoard : Scoreboard
+    {
+        private List<RankedPlayer> highScores;
+        private int AmountOfHighScores = 10;
+
+
+        public HighScoreBoard()
+        {
+            this.highScores = new List<RankedPlayer>();
+            //read files in parameters of highscores
+        }
+
+        public void AddPlayerTohighScoreBoard(String name, int score)
+        {
+            RankedPlayer aPlayer = new RankedPlayer(name, score);
+
+           this.highScores.Add(aPlayer);
+
+            this.sortHighScores();
+
+            this.trimHighScoreCollection();
+        }
+
+        private void trimHighScoreCollection()
+        {
+            while (this.highScores.Count > this.AmountOfHighScores)
+            {
+                this.highScores.RemoveAt(9);
+            }
+        }
+
+        private void sortHighScores()
+        {
+            var copyOfHighScores = this.highScores.OrderBy(player => player.Score);
+
+            this.highScores.Clear();
+
+            foreach (var variable in copyOfHighScores)
+            {
+                this.highScores.Add(variable);
+            }
+        }
+    }
+}
---
+++ SpaceInvaders/Model/RankedPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace SpaceInvaders.Model
+{
+    class RankedPlayer
+    {
+        private readonly int playerScore;
+        private readonly String playerIdentification;
+
+   
+        public RankedPlayer(int score) : this("Anonymous", score)
+        {
+        }
+
+        public RankedPlayer(String name, int score)
+        {
+            this.playerScore = score;
+            this.playerIdentification = name + ": " + score;
+        }
+
+
+        #region Property
+
+        public String PlayerID => this.playerIdentification;
+        public int Score => this.playerScore;
+
+        #endregion
+    }
+}
